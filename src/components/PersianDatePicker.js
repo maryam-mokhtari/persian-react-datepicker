b6597ad32709e3e99ac36361a63e50b722db7d6c
Maryam <maryam.mokhtari@gmail.com>
2021-05-20 12:54:07 +0430

    Start DatePicker

---
+++ src/components/PersianDatePicker.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const PersianDatePicker= ({ onSubmit, maxBirthYear }) => {
+
+  return (
+    <>hi
+     
+    </>
+  )
+}
+
+export default PersianDatePicker
