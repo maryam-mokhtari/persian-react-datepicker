905003618be5d2d1939aedeae392eeda70b29ed8
Maryam <maryam.mokhtari@gmail.com>
2021-05-20 16:06:02 +0430

    Indentation

--- src/components/PersianDatePicker.js
+++ src/components/PersianDatePicker.js
@@ -4,7 +4,7 @@ import moment from 'jalali-moment';
 import months from '../utils/months';
 import localeText from '../utils/locale';
 
-const PersianDatePicker= ({ onSubmit, maxBirthYear=1390 }) => {
+const PersianDatePicker = ({ onSubmit, maxBirthYear = 1390 }) => {
   const [day, setDay] = useState()
   const [month, setMonth] = useState()
   const [year, setYear] = useState()
@@ -28,51 +28,51 @@ const PersianDatePicker= ({ onSubmit, maxBirthYear=1390 }) => {
   const scrollMonth = e => {
     e.stopPropagation()
   }
-  
+
   const scrollYear = e => {
     e.stopPropagation()
   }
 
   return (
-   <div className="date-picker">
-        <div className="select" onScroll={e => scrollDay(e) }>
-          {Array(daysRange).fill().map((d, index) =>
-            <div className="option"
-              key={index}
-              className={`option ${day === index + 1 ? 'selected' : ''}`}
-              onClick={e => {
-                setDay(index + 1)
-              }}>
-              {index + 1}
-            </div>
-          )}
-        </div>
-        <div className="select" onScroll={e => scrollMonth(e) }>
-          {months.map((m, index) =>
-            <div
-              key={index}
-              className={`option ${month === index + 1 ? 'selected' : ''}`}
-              onClick={e => {
-                setMonth(index + 1)
-              }}>
-              {localeText[m]}
-            </div>
-          )}
-        </div>
-        <div className="select" onScroll={e => scrollYear(e) }>
-          {Array(maxBirthYear - 1300).fill().map((y, index) =>
-            <div
-              key={index}
-              className={`option ${year === maxBirthYear - index ? 'selected' : ''}`}
-              onClick={e => {
-                setYear(maxBirthYear - index)
-              }}>
-              {maxBirthYear - index}
-            </div>
-          )
-        }
-        </div>
+    <div className="date-picker">
+      <div className="select" onScroll={e => scrollDay(e)}>
+        {Array(daysRange).fill().map((d, index) =>
+          <div className="option"
+            key={index}
+            className={`option ${day === index + 1 ? 'selected' : ''}`}
+            onClick={e => {
+              setDay(index + 1)
+            }}>
+            {index + 1}
+          </div>
+        )}
       </div>
+      <div className="select" onScroll={e => scrollMonth(e)}>
+        {months.map((m, index) =>
+          <div
+            key={index}
+            className={`option ${month === index + 1 ? 'selected' : ''}`}
+            onClick={e => {
+              setMonth(index + 1)
+            }}>
+            {localeText[m]}
+          </div>
+        )}
+      </div>
+      <div className="select" onScroll={e => scrollYear(e)}>
+        {Array(maxBirthYear - 1300).fill().map((y, index) =>
+          <div
+            key={index}
+            className={`option ${year === maxBirthYear - index ? 'selected' : ''}`}
+            onClick={e => {
+              setYear(maxBirthYear - index)
+            }}>
+            {maxBirthYear - index}
+          </div>
+        )
+        }
+      </div>
+    </div>
   )
 }
 
